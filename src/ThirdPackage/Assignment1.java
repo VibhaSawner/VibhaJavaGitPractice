d4cbc892768223874d858dc8ee88e56d46ca19e6
Vibha Sawner <vibha23sawner@gmail.com>
2026-08-19 17:16:03 +0530

    this is vibha third package

---
+++ src/ThirdPackage/Assignment1.java
@@ -0,0 +1,16 @@
+package ThirdPackage;
+
+public class Assignment1 {
+
+	
+	
+	
+	public static void main(String[] args) {
+		String[] arr = {"Maulik", "Kanani","Mahesh", "Kanani"};
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	
+}
+}
