82cd2cd279a1a43b4b75fa1a42a6c3c0d2ace639
Vibha Sawner <vibha23sawner@gmail.com>
2026-08-21 12:17:07 +0530

    second time Branch A

--- src/ThirdPackage/Assignment1.java
+++ src/ThirdPackage/Assignment1.java
@@ -10,9 +10,10 @@ public class Assignment1 {
 		
 		for(int i=0;i<arr.length;i++) {
 		System.out.println("hi added branch A");
-			System.out.println(arr[i]+"chnages on branch B");
+			System.out.println(arr[i]+"SecondTime changes on line no 13");
 		}
 		System.out.println("Chnages on branch B");
 		System.out.println("Chnages on branch B added new line");
+		System.out.println("SecondTime changes line added on 17");
 }
 }
